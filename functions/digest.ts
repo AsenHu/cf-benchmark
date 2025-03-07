31b622038cd3e3cd8397925d6669d2e024427a19
AsenHu <78863300+AsenHu@users.noreply.github.com>
2025-03-07 16:50:19 +0000

    我就不信测不出来耗时

--- functions/digest.ts
+++ functions/digest.ts
@@ -1,68 +1,21 @@
 const onRequestGet = async (context: { request: Request }) => {
-    // 提取测试数据大小
-    const times = new URL(context.request.url).searchParams.get('times') || '1000';
-    const timesInt = parseInt(times);
-    if (isNaN(timesInt) || timesInt < 1) {
-        return new Response('Invalid times', {
-            status: 400,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Content-Type': 'text/plain'
-            }
-        });
+    // 提取测试信息
+    const algorithm = new URL(context.request.url).searchParams.get('algorithm');
+    const data = new URL(context.request.url).searchParams.get('data');
+    const times = new URL(context.request.url).searchParams.get('times') || '1';
+
+    const start = performance.now();
+    for (let i = 0; i < parseInt(times); i++) {
+        const hash = await crypto.subtle.digest(algorithm, new TextEncoder().encode(data));
+        const hashstr = Array.from(new Uint8Array(hash)).map(b => b.toString(16).padStart(2, '0')).join('');
+        const spent = performance.now() - start;
+        console.log(`[${i + 1}/${times}] ${algorithm}(${data}) = ${hashstr} (${spent}ms)`);
     }
 
-    // 生成数据
-    const data = new ArrayBuffer(134217727); // 128 MiB
-
-    // MD5
-    let start = Date.now();
-    for (let i = 0; i < timesInt; i++) {
-        await crypto.subtle.digest('MD5', data);
-    }
-    const md5Time = Date.now() - start;
-
-    // SHA-1
-    start = Date.now();
-    for (let i = 0; i < timesInt; i++) {
-        await crypto.subtle.digest('SHA-1', data);
-    }
-    const sha1Time = Date.now() - start;
-
-    // SHA-2 256 bits
-    start = Date.now();
-    for (let i = 0; i < timesInt; i++) {
-        await crypto.subtle.digest('SHA-256', data);
-    }
-    const sha256Time = Date.now() - start;
-
-    // SHA-2 384 bits
-    start = Date.now();
-    for (let i = 0; i < timesInt; i++) {
-        await crypto.subtle.digest('SHA-384', data);
-    }
-    const sha384Time = Date.now() - start;
-
-    // SHA-2 512 bits
-    start = Date.now();
-    for (let i = 0; i < timesInt; i++) {
-        await crypto.subtle.digest('SHA-512', data);
-    }
-    const sha512Time = Date.now() - start;
-
-    // 生成返回数据
-    const resp = JSON.stringify({
-        times: timesInt,
-        md5: md5Time,
-        sha1: sha1Time,
-        sha2_256: sha256Time,
-        sha2_384: sha384Time,
-        sha2_512: sha512Time
-    });
-    return new Response(resp, {
+    return new Response(null, {
+        status: 200,
         headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
+            'Access-Control-Allow-Origin': '*'
         }
     });
 }
